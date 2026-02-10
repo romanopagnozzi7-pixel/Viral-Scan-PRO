ad5158b148e9137251679b1ba405b9bd95d85c13
romanopagnozzi7-pixel <romanopagnozzi7@gmail.com>
2026-02-10 21:16:04 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import google.generativeai as genai
+import google.generativeai as genai #type ignore
 
 # Setup Gemini using your Streamlit Secrets
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
@@ -7,3 +7,4 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 
 st.title("Viral Scan PRO")
 st.write("App is now running.")
+
